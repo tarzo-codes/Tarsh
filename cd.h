74e48baf471094a93343c5bad50ca016b051f703
tarzo-codes <techressolve1@gmail.com>
2026-06-15 10:52:40 +0600

    Clean slate

--- cd.h
+++
@@ -1,7 +0,0 @@
-#ifndef CD_H
-#define CD_H
-
-// The blueprint: takes the arguments array AND the prompt buffer to update it
-void handle_cd(char **args, char *prompt, int prompt_size);
-
-#endif
--- echo.h
+++
@@ -1,7 +0,0 @@
-#ifndef ECHO_H
-#define ECHO_H
-
-void handleEcho(char **args);
-
-#endif // !ECHO_H
-
