264b02acdc06d28f2188841c6978f700fa4353a0
tarzo-codes <techressolve1@gmail.com>
2026-06-15 12:32:21 +0600

    added a few error hanndlings

--- src/main.c
+++ src/main.c
@@ -2,21 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "parser.h"
 
 int main() {
-    char *command_buffer;
-    size_t n = 0;
+    char *command_buffer = NULL;
+    size_t buffer_size = 0;
     while (true) {
     
     printf(" $ ");
-    getline(&command_buffer,&n,stdin);
+    ssize_t charecter_read = getline(&command_buffer,&buffer_size,stdin);
     printf("%s",command_buffer);
     
+    // NOTE: Error Handling
+    // Handling ctrl+D EOF 
+    if (charecter_read == -1) {
+      printf("\n EOF detecterd \n Exitting.... \n");
+      break;
+    }
+    
     command_buffer[strcspn(command_buffer,"\n")] = '\0';
-
+    
+    // Handle Empty inputs
+    if (strlen(command_buffer) == 0) {
+      continue;
+    } 
     if (strcmp(command_buffer,"exit") == 0) {
       printf("Exited the shell");
       break;
     }
+    // NOTE: Parsing commands and separating arguments
+
+    char **args = malloc(64 * sizeof(char *));
+    
+    if (args == NULL) {
+      perror("Fatal: Allocation failure at loop start");
+      exit(EXIT_FAILURE);
+    }
+
+    main_command_parser(command_buffer, args);
+
+    printf("[Parser status] Parser Arguments: \n");
+    for (int i = 0;args[i] != NULL;i++) {
+      printf("  -> Arguments[%d]: %s\n",i,args[i]);
+    }
+    free(args);
   }
+  free(command_buffer);
+  return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "parser.h"
+
 
 char** main_command_parser(char *buffer, char **args) {
   // TODO: Take the command and tokenize it then put it in the args pointer
   
   char *buffer_tok = strtok(buffer," ");
   int tok_counter = 0;
+  int max_slots = 64;
+
   while (buffer_tok != NULL) {
+    if (tok_counter >= max_slots - 1) {
+      max_slots *= 2;
+      args = realloc(args, max_slots * sizeof(char *));
+      if (args == NULL) {
+        perror("Fatal: Shell ran out of heap memory during parsing");
+        exit(EXIT_FAILURE);
+      }
+    }
     args[tok_counter] = buffer_tok;
     buffer_tok = strtok(NULL," ");
     tok_counter++;
